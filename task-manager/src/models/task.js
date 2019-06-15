307533b14c5c483c8a9af9dfb63f72bde79aecb0
MarcoSants <marcochel@gmail.com>
2019-06-15 17:00:47 -0300

    Lessons: 117->123

--- task-manager/src/models/task.js
+++ task-manager/src/models/task.js
@@ -1,19 +1,26 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Task = mongoose.model('Task', {
+const taskSchema = new mongoose.Schema(
+  {
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     completed: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User"
     }
-})
+  },
+  {
+    timestamps: true //Automatically adds created/updated
+  }
+);
 
-module.exports = Task
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -1,40 +1,62 @@
-const express = require('express')
-const Task = require('../models/task')
-const auth = require('./../middlewares/auth')
+const express = require("express");
+const Task = require("../models/task");
+const auth = require("./../middlewares/auth");
 
-const router = new express.Router()
+const router = new express.Router();
 
-router.post('/tasks', auth, async (req, res) => {
-    try {
-        const task = new Task({
-            ...req.body,
-            owner: req.user._id
-        })
-        await task.save()
-        res.status(201).send(task)
-    } catch (error) {
-        res.status(400).send(error)
+router.post("/tasks", auth, async (req, res) => {
+  try {
+    const task = new Task({
+      ...req.body,
+      owner: req.user._id
+    });
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// GET /tasks?completed=true  query
+// GET /taks?limit=10&skip=0  pagination
+// GET /tasks?sortBy=createdAt:desc
+router.get("/tasks", auth, async (req, res) => {
+  try {
+    const match = {};
+    const sort = {}; //{createdAt: 1 // 1: asc, -1: desc}
+    if (req.query.completed) match.completed = req.query.completed === "true";
+    if (req.query.sortBy) {
+      const parts = req.query.sortBy.split(":");
+      sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
     }
-})
+    await req.user
+      .populate({
+        path: "tasks",
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort
+        }
+      })
+      .execPopulate();
+    res.send(req.user.tasks);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
-router.get('/tasks', auth, async (req, res) => {
-    try {
-        //const tasks = await Task.find({ owner: req.user._id })
-        await req.user.populate('tasks').execPopulate()
-        res.send(tasks)
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
+router.get("/tasks/:id", auth, async (req, res) => {
+  try {
+    const task = await Task.findOne({
+      _id: req.params.id,
+      owner: req.user._id
+    });
+    if (!task) res.status(404).send();
+    res.send(task);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
-router.get('/tasks/:id', auth, async (req, res) => {
-    try {
-        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
-        if (!task) res.status(404).send()
-        res.send(task)
-    } catch (error) {
-        res.status(500).send(error)
-    }
-})
-
-module.exports = router
+module.exports = router;
